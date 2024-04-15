26f9d25b2d6ea70ced202572de133d968b27147e
Robby Widyahartono <robby.widyahartono@komunal.co.id>
2024-04-16 06:53:28 +0700

    integrate with daisy ui

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,9 @@
-import * as React from 'react'
-
+import Link from 'next/link'
 const AboutPage = () => {
   return (
     <div>
       <h1 className='text-7xl'>About Page</h1>
+      <Link href='/' className='text-2xl'>Home Page</Link>
     </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import * as React from 'react'
+import Link from 'next/link'
 
 const HomePage = () => {
   return (
     <div>
       <h1 className='text-7xl'>Home Page</h1>
+      <Link href='/about' className='text-2xl'>About Page</Link>
     </div>
   )
 }

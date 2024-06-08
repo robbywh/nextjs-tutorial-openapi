77d6f46c969d222114bcd5b4661b2eb37db52fcf
Robby Widyahartono <robby.widyahartono@komunal.co.id>
2024-06-08 21:50:56 +0700

    integrate with prisma

--- src/app/drinks/[id]/page.tsx
+++ src/app/drinks/[id]/page.tsx
@@ -1,7 +1,30 @@
-const SingleDrinkPage = ({params}: any) => {
-  console.log(params.id)
+import Link from 'next/link';
+import Image from 'next/image';
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
+// import drinkImg from './drink.jpg';
+
+const getSingleDrink = async (id: string) => {
+  const res = await fetch(`${url}${id}`)
+  if (!res.ok) {
+    throw new Error('Failed to fetch a drink...');
+  }
+  return res.json();
+}
+const SingleDrinkPage = async ({params}: any) => {
+  const data = await getSingleDrink(params.id);
+  const title = data?.drinks[0]?.strDrink;
+  const imgSrc = data?.drinks[0]?.strDrinkThumb;
   return (
-    <div>SingleDrinkPage</div>
+    <div>
+      <Link href='/drinks' className='btn btn-primary mt-8 mb-12'>
+        back to drinks
+      </Link>
+      <Image src={imgSrc} width={300} height={300} className='w-48 h-48 rounded-lg shadow-lg mb-4' priority alt={title} />
+      {/* <Image src={drinkImg} className='w-48 h-48 rounded-lg' alt='drink'/> */}
+      <h1 className='text-4xl mb-8'>
+        {title}
+      </h1>
+    </div>
   )
 }
 
